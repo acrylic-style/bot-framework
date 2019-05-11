33a27b2a01e7bae73261d79662c0863032b055ec
acrylic-style <me@acrylicstyle.xyz>
2019-05-12 04:16:35 +0900

    for resolve issues

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const Command = require('./src/structures/Command')
 const Resolver = require('./src/structures/Resolver')
 
-const dispatcher = require('./src/dispatcher')
-const commands = require('./src/commands')
-
 module.exports = {
   Command,
   Resolver,
-
-  dispatcher,
-  commands,
 }
